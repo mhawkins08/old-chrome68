6c269219205fedcbb70e32cee39364a787fe9720
Mason Hawkins <112046394+mhawkins08@users.noreply.github.com>
2022-09-21 16:20:38 -0400

    Add files via upload

---
+++ oldchrome.cpp
@@ -0,0 +1,7 @@
+// originally this contained some extra patches but they are no longer needed as they were patched into chrome.dll
+/*
+
+   OLDCHROME IS A PROJECT DESIGNED BY MASON HAWKINS
+   ANYBODY CAN USE / EDIT OLDCHROME AT THEIR PERSONAL DESIRE AS ALL RIGHTS TO GOOGLE CHROME BELONG TO GOOGLE.
+
+*/
